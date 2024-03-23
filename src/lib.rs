467e6593197844457bbe786aec75f6e486c5c442
Jonathan "Nath" Schild <nath@j-schild.de>
2024-03-23 20:00:37 +0100

    Add database connection

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,38 @@
 
 use actix_web::{
     middleware::{Compress, Logger, NormalizePath, TrailingSlash},
+    web::Data,
     App, HttpServer,
 };
+use dotenv::var;
 use log::info;
+use sqlx::PgPool;
+
+struct AppData {
+    pub _db: PgPool,
+}
 
 pub async fn diiudicatio_run() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     info!("{}", build_info());
+
+    let postgres_connection_string =
+        var("DATABASE_URL").expect("env variable `DATABASE_URL` must be set");
+
+    let postgres = PgPool::connect(&postgres_connection_string)
+        .await
+        .expect("cannot connect to PostgreSQL");
+    info!("connected to database");
 
     HttpServer::new(move || {
         App::new()
             .wrap(Compress::default())
             .wrap(Logger::default())
             .wrap(NormalizePath::new(TrailingSlash::Trim))
+            .app_data(Data::new(AppData {
+                _db: postgres.clone(),
+            }))
+        // .service(scope("/{vote_scope}").configure(vote::configure))
     })
     .bind(("127.0.0.1", 8000))
     .expect(&format!("could not bind to {}", 8000))
